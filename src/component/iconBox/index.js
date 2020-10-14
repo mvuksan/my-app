c8a178774b080b000c0662efdbd3add4bf7972cb
Milan Vuksan <milan.vuksan@ems.local>
2020-10-14 11:17:54 +0200

    new

---
+++ src/component/iconBox/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+const IconBox = () => (
+  <div className="ui-icon-box">
+    <div className="ui-box-image-label-wrapper">
+      <div className="ui-icon-box-label">
+        <p>Subscriber</p>
+      </div>
+    </div>
+    <div className="ui-icon-box-text">
+      <p>John Miller</p>
+    </div>
+  </div>
+);
+
+export default IconBox;
---
+++ src/containers/subscribers/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import IconBox from "../../iconBox";
+
+function Subscribers() {
+  return (
+    <div className="ui-top.content">
+      <div className="ui-top-boxes">
+        <IconBox />
+        <IconBox />
+        <IconBox />
+      </div>
+    </div>
+  );
+}
+
+export default Subscribers;
