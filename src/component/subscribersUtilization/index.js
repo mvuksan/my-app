84cdc6881733072c16cbffee8117abb99f1f6963
Milan Vuksan <milan.vuksan@ems.local>
2020-10-20 13:49:47 +0200

    progressbar

---
+++ src/component/subscribersUtilization/index.js
@@ -0,0 +1,69 @@
+import "./style.css";
+import React from "react";
+import DownloadArrow from "../../assets/images/arrow-dl.png";
+import UploadArrow from "../../assets/images/arrow-up.png";
+
+const SubscribersUtilization = ({}) => (
+  <div id="utilization" className="ui-box ui-progress-box">
+    <div className="ui-speed">
+      <div className="ui-speed-inner">
+        <div className="ui-speed-value-container">
+          <div>
+            <img
+              className="ui-speed-arrow"
+              src={DownloadArrow}
+              alt="Download arrow"
+            />
+          </div>
+          <div>
+            <p className="ui-speed-label-green">21.4 Mb/s</p>
+          </div>
+        </div>
+        <div className="ui-progress-gradient ui-gradient-green">
+          <div className="ui-progress-number-container">
+            <div>
+              <p>1</p>
+            </div>
+            <div>
+              <p>5</p>
+            </div>
+            <div>
+              <p>20</p>
+            </div>
+          </div>
+          <span className="ui-progress-background"></span>
+        </div>
+      </div>
+      <div className="ui-speed-inner ">
+        <div className="ui-speed-value-container">
+          <div>
+            <img
+              className="ui-speed-arrow"
+              src={UploadArrow}
+              alt="Upload arrow"
+            />
+          </div>
+          <div>
+            <p className="ui-speed-label-blue">4.8 Mb/s</p>
+          </div>
+        </div>
+        <div className="ui-progress-gradient ui-gradient-blue">
+          <div className="ui-progress-number-container">
+            <div>
+              <p>1</p>
+            </div>
+            <div>
+              <p>5</p>
+            </div>
+            <div>
+              <p>20</p>
+            </div>
+          </div>
+          <span className="ui-progress-background"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default SubscribersUtilization;
--- src/containers/subscribers/index.js
+++ src/containers/subscribers/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import IconBox from "../../component/iconBox";
+import SubscribersUtilization from "../../component/subscribersUtilization";
 
 function Subscribers() {
   return (
@@ -8,6 +9,9 @@ function Subscribers() {
         <IconBox />
         <IconBox />
         <IconBox />
+      </div>
+      <div className="ui-center-content">
+        <SubscribersUtilization />
       </div>
     </div>
   );
